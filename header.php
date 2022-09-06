d4603cfeeac553b2f7bf2d28401917471f5f9653
Emydon12 <emekadonatus30@gmail.com>
2022-09-06 16:45:13 +0100

    Testing

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php
+<!doctype html>
+<html lang = "en">
+    <head>
+        <title>Global Solar World</title>
+</head>
+<body>
+    <h1> just testing how this text editor works</h1>
+</body>
+</html>    
+?>
